3b1dd31d366e7cc9a8fbdd6604928913479ac435
marcocspc <marcocspc@hotmail.com>
2019-09-15 22:18:08 +0000

    MiniRTS added, needs testing

---
+++ urnai/envs/mini-rts.py
@@ -0,0 +1,81 @@
+from .base.abenv import Env
+from rlpytorch import load_module 
+from os.path import expanduser
+import sys
+
+class MiniRTSEnv(Env):
+
+    #TODO: TEST THIS!!!!!
+
+    def __init__(self, minirts_path = expanduser("~") + "/ELF/rts/game_MC/game.py"):
+        '''
+            Initizalize attributes
+        '''
+        #remove .py from minirts path
+        if minirts_path.endswith('.py'):
+            minirts_path = minirts_path[:-3]
+
+        try:
+            game = load_module(minirts_path).Loader()
+
+        self.rts = game.initialize()
+
+        self.observation = None
+        self.actoraction = None
+        self.done = True
+        self.chosen_action = self._act_callback
+
+    def start(self):
+        '''
+            Register action callback
+            Start minirts
+        '''
+
+        self.rts.reg_callback("actor", self.chosen_action)
+        self.rts.Start()
+        self.done = False
+
+    def step(self, action):
+        '''
+            Executes action and return observation, done
+        '''
+
+        self.chosen_action = action
+        self.rts.Run()
+
+        return self.observation(),self.done
+
+    def close(self):
+        '''
+            Stops minirts
+        '''
+        self.rts.Stop()
+        self.done = True
+
+    def reset(self):
+        '''
+            Restart environment and returns initial observation
+            TODO: check if self.start() modifies self.observation value
+        '''
+
+        self.close()
+        self.observation = None
+        self.actoraction = None
+        self.start()
+
+        return self.observation
+
+    def restart(self):
+        self.reset()
+
+
+    def _act_callback(self, batch):
+        self.observation = batch
+        return self.actoraction
+
+
+
+
+
+
+
