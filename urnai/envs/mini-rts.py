a61958e974e3db87ff221403b6fc5657f8e5a8d8
marcocspc <marcocspc@hotmail.com>
2019-09-17 20:29:46 +0000

    ai added to mini-rts, need testing though

--- urnai/envs/mini-rts.py
+++ urnai/envs/mini-rts.py
@@ -1,18 +1,17 @@
 from .base.abenv import Env
-from rlpytorch import load_module 
+from rlpytorch import load_module, argparse 
 from os.path import expanduser
 import sys
 
 class MiniRTSEnv(Env):
 
-    #TODO: add enemy AI to environment, check: 
+    #TODO: add enemy AI to environment, check: MIGHT HAVE BEEN DONE, NEED TO BE TESTED 
     #https://github.com/facebookresearch/ELF/blob/master/train_minirts.sh and
     #https://github.com/facebookresearch/ELF/blob/master/eval_minirts.sh and
     #https://github.com/facebookresearch/ELF/blob/master/eval.py
-    #TODO: add option to render env or not
     #TODO: TEST THIS!!!!!
 
-    def __init__(self, minirts_path = expanduser("~") + "/ELF/rts/game_MC/game.py", render = False, enemyAI = True):
+    def __init__(self, minirts_path = expanduser("~") + "/ELF/rts/game_MC/game.py", enemyAI = True, ai_fskip = 50, model_fskip = 50):
         '''
             Initizalize attributes
         '''
@@ -20,8 +19,12 @@ class MiniRTSEnv(Env):
         if minirts_path.endswith('.py'):
             minirts_path = minirts_path[:-3]
 
-        try:
-            game = load_module(minirts_path).Loader()
+        game = load_module(minirts_path).Loader()
+
+        #This might add an AI to the game
+        argparse.ArgumentParser()
+        cmd = '--num_games 1024 --batchsize 128 --tqdm --load ${MODEL} --gpu 0 --players "fs=' + str(model_fskip) + ',type=AI_NN;fs=+' str(ai_fskip) ' +,type=AI_SIMPLE" --eval_stats winrate --num_eval $NUM_EVAL --additional_labels id,last_terminal,seq --shuffle_player --num_frames_in_state 1 --greedy --keys_in_reply V'
+        game.args.Load(argparse.ArgumentParser(), game, cmd_line=cmd)
 
         self.rts = game.initialize()
 
