1e9354db4a97c56aad4a8d1192c207ba75717e63
marcocspc <marcocspc@hotmail.com>
2019-11-27 23:17:53 -0300

    updated DeepRTS env, needs testing

--- urnai/envs/deep-rts.py
+++ urnai/envs/deep-rts.py
@@ -1,29 +1,53 @@
-from pyDeepRTS import Game
 from .base.abenv import Env
 import os
+from DeepRTS import Engine
+from DeepRTS.python import scenario
+from DeepRTS.python import Config
+from DeepRTS.python import Game
 
 #TODO: add a way to view game main window if render = True
 #TODO: add enemy AI
 
 class DeepRTSEnv(Env):
+
+    '''
+       DeepRTS.python.Config Defaults: https://github.com/cair/deep-rts/blob/master/DeepRTS/python/_py_config.py
+       DeepRTS.Engine.Config.defaults() : https://github.com/cair/deep-rts/blob/master/src/Config.h
+
+    '''
     
-    def __init__(self), map = '10x10-2v2.json', render = False):
+    def __init__(self, map = Config.Map.TEN, render = False, 
+            max_fps = 1000000, max_ups = 1000000, play_audio = False, 
+            number_of_players = 1, updates_per_action = 1, flatten_state = True):
 
         self.map = map
         self.render = render
+        self.play_audio = play_audio
+        self.updates_per_action = updates_per_action
+        self.flatten_state = flatten_state
 
         if (os.path.isdir('./assets')):
-            #Setup game, informing map
-            self.game = Game(self.map)
+            self.gui_config = python.Config(
+                render=self.render,
+                view=self.render,
+                inputs=False,
+                caption=False,
+                unit_health=True,
+                unit_outline=True,
+                unit_animation=True,
+                audio=self.play_audio,
+                audio_volume=50
+            )
 
-            #Add two players
-            self.player1 = self.game.add_player()
-            self.player2 = self.game.add_player()
+            self.engine_config = Engine.Config.defaults()
 
-            #Set FPS and UPS limits
-            self.game.set_max_fps(10000000)
-            self.game.set_max_ups(10000000)
-
+            self.game = Game(
+                self.map,
+                n_players = self.number_of_players,
+                engine_config = self.engine_config,
+                gui_config = self.gui_config,
+                terminal_signal = False
+            )
         else:
             raise Exception("Directory 'assets' not found. Please obtain it using:\n\n" +
                     "git clone https://github.com/cair/deep-rts.git")
@@ -34,23 +58,33 @@ class DeepRTSEnv(Env):
 
         #Start DeepRTS
         self.game.start()
+        self.game.reset()
+
 
     def step(self, action):
-        #Update game clock
-        self.game.tick()
+        #select first player
+        #you can do self.game.selected_player
+        player = self.game.players[0]
 
-        #Process game state
-        self.game.update()
+        #make player do that action
+        player.do_action(action)
 
-        #If render is enabled, draw state to graphic
-        if (self.render):
-            self.game.render()
+        #update game state
+        for i in range(self.updates_per_action):
+            self.game.update()
 
-        #Get current state
-        state = self.game.state
+        #get game state, flattened or not
+        state = None
+        if self.flatten_state:
+            state = self.game.get_state().flatten()
+        else:
+            state = self.game.get_state()
 
-        #Make player1 act
-        self.player1.do_action(action)
+        #make game update its internal graphics
+        #and show windows, if it was configured to
+        self.game.render()
+        if self.render:
+            self.game.view()
 
         #Return observation and done
         return state, self.done
