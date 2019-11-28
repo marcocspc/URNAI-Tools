31ab26facf18cf948c0fa722d1bf219806380f42
marcocspc <marcocspc@hotmail.com>
2019-11-27 23:42:53 -0300

    testing deeprts

---
+++ urnai/envs/deep_rts.py
@@ -0,0 +1,104 @@
+from .base.abenv import Env
+import os
+from DeepRTS import Engine
+from DeepRTS.python import scenario
+from DeepRTS.python import Config
+from DeepRTS.python import Game
+
+#TODO: add a way to view game main window if render = True
+#TODO: add enemy AI
+
+class DeepRTSEnv(Env):
+
+    '''
+       DeepRTS.python.Config Defaults: https://github.com/cair/deep-rts/blob/master/DeepRTS/python/_py_config.py
+       DeepRTS.Engine.Config.defaults() : https://github.com/cair/deep-rts/blob/master/src/Config.h
+       Possible actions: https://github.com/cair/deep-rts/blob/master/src/Constants.h
+    '''
+    
+    def __init__(self, map = Config.Map.TEN, render = False, 
+            max_fps = 1000000, max_ups = 1000000, play_audio = False, 
+            number_of_players = 1, updates_per_action = 1, flatten_state = True):
+
+        self.map = map
+        self.render = render
+        self.play_audio = play_audio
+        self.updates_per_action = updates_per_action
+        self.flatten_state = flatten_state
+
+        if (os.path.isdir('./assets')):
+            self.gui_config = python.Config(
+                render=self.render,
+                view=self.render,
+                inputs=False,
+                caption=False,
+                unit_health=True,
+                unit_outline=True,
+                unit_animation=True,
+                audio=self.play_audio,
+                audio_volume=50
+            )
+
+            self.engine_config = Engine.Config.defaults()
+
+            self.game = Game(
+                self.map,
+                n_players = self.number_of_players,
+                engine_config = self.engine_config,
+                gui_config = self.gui_config,
+                terminal_signal = False
+            )
+        else:
+            raise Exception("Directory 'assets' not found. Please obtain it using:\n\n" +
+                    "git clone https://github.com/cair/deep-rts.git")
+
+    def start(self):
+        #Set done
+        self.done = False
+
+        #Start DeepRTS
+        self.game.start()
+        self.game.reset()
+
+
+    def step(self, action):
+        #select first player
+        #you can do self.game.selected_player
+        player = self.game.players[0]
+
+        #make player do that action
+        player.do_action(action)
+
+        #update game state
+        for i in range(self.updates_per_action):
+            self.game.update()
+
+        #get game state, flattened or not
+        state = None
+        if self.flatten_state:
+            state = self.game.get_state().flatten()
+        else:
+            state = self.game.get_state()
+
+        #make game update its internal graphics
+        #and show windows, if it was configured to
+        self.game.render()
+        if self.render:
+            self.game.view()
+
+        #Return observation and done
+        return state, self.done
+
+    def close(self):
+        #Stop DeepRTS
+        self.game.stop()
+
+        #Set done
+        self.done = True
+
+    def reset(self):
+        self.close()
+        self.start()
+
+    def restart(self):
+        self.reset()
---
+++ urnai/envs/mini_rts.py
@@ -0,0 +1,129 @@
+#from .base.abenv import Env
+from os.path import expanduser
+import argparse
+import sys
+
+#class MiniRTSEnv(Env):
+class MiniRTSEnv():
+
+    #TODO: add enemy AI to environment, check: MIGHT HAVE BEEN DONE, NEED TO BE TESTED 
+    #https://github.com/facebookresearch/ELF/blob/master/train_minirts.sh and
+    #https://github.com/facebookresearch/ELF/blob/master/eval_minirts.sh and
+    #https://github.com/facebookresearch/ELF/blob/master/eval.py
+    #TODO: TEST THIS!!!!!
+
+    def __init__(self, elf_path = expanduser("~") + "/ELF", enemyAI = True, ai_fskip = 50, model_fskip = 50, gpu = False, num_games = 1, max_ticks = 1000):
+
+        raise Exception("Mini-RTS is not working yet. Game freezes. Until we work around it, this is exception is raised.")
+
+        '''
+            Initizalize attributes
+
+            gpu = True #needs CUDA
+
+            rts usage: game.py [-h] [--handicap_level HANDICAP_LEVEL] [--players PLAYERS]
+            [--max_tick MAX_TICK] [--shuffle_player]
+            [--num_frames_in_state NUM_FRAMES_IN_STATE]
+            [--max_unit_cmd MAX_UNIT_CMD] [--seed SEED] [--actor_only]
+            [--model_no_spatial]
+            [--save_replay_prefix SAVE_REPLAY_PREFIX]
+            [--output_file OUTPUT_FILE]
+            [--cmd_dumper_prefix CMD_DUMPER_PREFIX] [--gpu GPU]
+            [--use_unit_action] [--disable_time_decay]
+            [--use_prev_units] [--attach_complete_info]
+            [--feature_type FEATURE_TYPE] [--num_games NUM_GAMES]
+            [--batchsize BATCHSIZE] [--game_multi GAME_MULTI] [--T T]
+            [--eval] [--wait_per_group]
+            [--num_collectors NUM_COLLECTORS] [--verbose_comm]
+            [--verbose_collector] [--mcts_threads MCTS_THREADS]
+            [--mcts_rollout_per_thread MCTS_ROLLOUT_PER_THREAD]
+            [--mcts_verbose]
+            [--mcts_save_tree_filename MCTS_SAVE_TREE_FILENAME]
+            [--mcts_verbose_time] [--mcts_use_prior]
+            [--mcts_pseudo_games MCTS_PSEUDO_GAMES]
+            [--mcts_pick_method MCTS_PICK_METHOD]
+            [--additional_labels ADDITIONAL_LABELS]
+        '''
+        #import ELF's rlpytorch
+        sys.path.append(elf_path)
+        from rlpytorch.model_loader import load_module
+
+        minirts_path = elf_path + "/rts/game_MC/game"
+        game = load_module(minirts_path).Loader()
+
+        #This might add an AI to the game
+        parser = argparse.ArgumentParser()
+        cmd = ["--num_games", str(num_games), "--batchsize", "128", "--players", "\"fs="+ str(model_fskip) + ",type=AI_NN;fs="+ str(ai_fskip) + ",type=AI_SIMPLE\"", "--additional_labels", "id,last_terminal,seq", "--shuffle_player", "--num_frames_in_state", "1", "--max_tick", str(max_ticks)]
+
+        if gpu:
+            cmd += ["--gpu", "0"]
+
+        game.args.Load(parser, game, cmd_line=cmd)
+
+        self.rts = game.initialize()
+
+        self.observation = None
+        self.actor_action = None
+        self.done = True
+        self.chosen_action = self._act_callback
+        self.dummy_train = self._dummy_train_function
+
+    def start(self):
+        '''
+            Register action callback
+            Start minirts
+        '''
+
+        self.rts.reg_callback("actor", self.chosen_action)
+        self.rts.reg_callback("train", self.dummy_train)
+        self.rts.Start()
+        self.done = False
+
+    def step(self, action):
+        '''
+            Executes action and return observation, done
+        '''
+
+        self.actor_action = action
+        self.rts.Run()
+
+        return self.observation,self.done
+
+    def close(self):
+        '''
+            Stops minirts
+        '''
+        self.rts.Stop()
+        self.done = True
+
+    def reset(self):
+        '''
+            Restart environment and returns initial observation
+            TODO: check if self.start() modifies self.observation value
+        '''
+
+        self.close()
+        self.observation = None
+        self.actor_action = None
+        self.start()
+
+        return self.observation
+
+    def restart(self):
+        self.reset()
+
+
+    def _act_callback(self, batch):
+        self.observation = batch
+        return self.actor_action
+
+    def _dummy_train_function(self, batch):
+        return
+
+
+print("URNAI: start minirts")
+rts = MiniRTSEnv()
+print("URNAI: run minirts")
+rts.rts.Run()
+print("URNAI: closing minirts")
+rts.close()
