7d444ded5316ce73bad55a254d098c0a63ff9cc2
marcocspc <marcocspc@hotmail.com>
2020-01-16 23:06:44 -0300

    fixing urnai cmd

--- urnai/runner/commands.py
+++ urnai/runner/commands.py
@@ -1,6 +1,7 @@
 from .base.runner import Runner
 import os
 
+import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir) 
