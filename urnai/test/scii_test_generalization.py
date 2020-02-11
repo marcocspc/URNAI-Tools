4f6dcef342870434ecef4030c2b323fcc707a48c
marcocspc <marcocspc@hotmail.com>
2020-02-10 23:39:40 -0300

    work in progress on generalization

--- urnai/test/scii_test_generalization.py
+++ urnai/test/scii_test_generalization.py
@@ -20,15 +20,15 @@ def print_army_mean(obs):
     xs = [unit.x for unit in army]
     ys = [unit.y for unit in army]
 
-    print("Army position is [{x}, {y}]".format(x=int(mean(xs)),y=int(mean(ys))))
+    #print("Army position is [{x}, {y}]".format(x=int(mean(xs)),y=int(mean(ys))))
 
 def move_left(obs):
     army = scaux.select_army(obs, sc2_env.Race.terran)
     xs = [unit.x for unit in army]
     ys = [unit.y for unit in army]
 
-    new_army_x = int(mean(xs))
-    new_army_y = int(mean(ys)) - HOR_THRESHOLD
+    new_army_x = int(mean(xs)) - HOR_THRESHOLD
+    new_army_y = int(mean(ys))
 
     for unit in army:
         PENDING_ACTIONS.append(actions.RAW_FUNCTIONS.Move_pt("now", unit.tag, [new_army_x, new_army_y]))
@@ -38,8 +38,8 @@ def move_right(obs):
     xs = [unit.x for unit in army]
     ys = [unit.y for unit in army]
 
-    new_army_x = int(mean(xs))
-    new_army_y = int(mean(ys)) + HOR_THRESHOLD
+    new_army_x = int(mean(xs)) + HOR_THRESHOLD
+    new_army_y = int(mean(ys))
 
     for unit in army:
         PENDING_ACTIONS.append(actions.RAW_FUNCTIONS.Move_pt("now", unit.tag, [new_army_x, new_army_y]))
@@ -49,8 +49,8 @@ def move_down(obs):
     xs = [unit.x for unit in army]
     ys = [unit.y for unit in army]
 
-    new_army_x = int(mean(xs)) + VER_THRESHOLD
-    new_army_y = int(mean(ys))
+    new_army_x = int(mean(xs))
+    new_army_y = int(mean(ys)) + VER_THRESHOLD
 
     for unit in army:
         PENDING_ACTIONS.append(actions.RAW_FUNCTIONS.Move_pt("now", unit.tag, [new_army_x, new_army_y]))
@@ -60,17 +60,20 @@ def move_up(obs):
     xs = [unit.x for unit in army]
     ys = [unit.y for unit in army]
 
-    new_army_x = int(mean(xs)) - VER_THRESHOLD
-    new_army_y = int(mean(ys))
+    new_army_x = int(mean(xs))
+    new_army_y = int(mean(ys)) - VER_THRESHOLD
 
     for unit in army:
         PENDING_ACTIONS.append(actions.RAW_FUNCTIONS.Move_pt("now", unit.tag, [new_army_x, new_army_y]))
 
+def no_op():
+    PENDING_ACTIONS.append(actions.RAW_FUNCTIONS.no_op())
+
+
 
 def main():
-#    players = [sc2_env.Agent(sc2_env.Race.terran), sc2_env.Bot(sc2_env.Race.random, sc2_env.Difficulty.very_easy)]
     players = [sc2_env.Agent(sc2_env.Race.terran)]
-    scii = SC2Env(map_name="CollectMineralShards", render=True, step_mul=16, players=players)
+    scii = SC2Env(map_name="CollectMineralShards", render=True, step_mul=32, players=players)
     episodes = 100
     steps = 99999999999999 
 
@@ -84,38 +87,41 @@ def main():
 
             action = None
 
-            if state == None:
-                action = actions.RAW_FUNCTIONS.no_op()
-                print('action was noop')
-            elif len(PENDING_ACTIONS) > 0:
-                action = PENDING_ACTIONS.pop()
-                print('chose pending action')
-            else:
+            if state != None and len(PENDING_ACTIONS) == 0:
                 #ask for direction
                 string = ''' Choose:
                 1 - Up
                 2 - Down
                 3 - Left
                 4 - Right
-'''
+                5 - No-op
+    '''
 
                 #need to insert a way to choose actions
-                action = int(input(string))
+                try:
+                    action = int(input(string))
+                except ValueError:
+                    action = 5
                 print('pending action list empty')
 
                 #check if it is not at the limit of screen
                 #move it to desired direction
                 if action == 1:
-                    action = move_up(state)
+                    move_up(state)
                 elif action == 2:
-                    action = move_down(state)
+                    move_down(state)
                 elif action == 3:
-                    action = move_left(state)
+                    move_left(state)
                 elif action == 4:
-                    action = move_right(state)
+                    move_right(state)
 
 
-            state, reward, done = scii.step([action])
+            state, reward, done = None, None, None
+            if len(PENDING_ACTIONS) > 0:
+                state, reward, done = scii.step([PENDING_ACTIONS.pop()])
+            else:
+                state, reward, done = scii.step([actions.RAW_FUNCTIONS.no_op()])
+
 
             #print("Current state: ")
             #print(state)
