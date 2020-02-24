69c0ccdf20b1f823f6fe232809365458bbd71b19
marcocspc <marcocspc@hotmail.com>
2020-02-24 18:30:48 -0300

    building first scenarios

--- urnai/scenarios/generalization/rts/collectables.py
+++ urnai/scenarios/generalization/rts/collectables.py
@@ -5,3 +5,10 @@ parentdir = os.path.dirname(parentdir)
 parentdir = os.path.dirname(parentdir)
 sys.path.insert(0,parentdir) 
 
+from scenario.base.abscenario import ABScenario
+
+class GeneralizedCollectablesScenario(ABScenario):
+
+    DEEP_RTS_GAME = 0
+
+    def __init__(self, game = GeneralizedCollectablesScenario.DEEP_RTS_GAME)
