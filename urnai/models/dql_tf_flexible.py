3635f3bd6664617f640d4064976571c251ef07b7
marcocspc <marcocspc@hotmail.com>
2020-03-24 22:00:20 -0300

    making some persistation adjustments

--- urnai/models/dql_tf_flexible.py
+++ urnai/models/dql_tf_flexible.py
@@ -18,6 +18,7 @@ from .model_builder.ModelBuilder import LAYER_INPUT, LAYER_FULLY_CONNECTED
 
 class DqlTfFlexible(LearningModel):
 
+    #Some useful class constants
     DEFAULT_BUILD_MODEL = [ 
         {
             'type' : LAYER_INPUT,
@@ -40,8 +41,8 @@ class DqlTfFlexible(LearningModel):
     ]
 
 
-    def __init__(self, action_wrapper: ActionWrapper, state_builder: StateBuilder, save_path='urnai/models/saved/', file_name='temp', learning_rate=0.0002, gamma=0.95, name='DQN', build_model = DqlTfFlexible.DEFAULT_BUILD_MODEL):
-        super(DQLTF, self).__init__(action_wrapper, state_builder, gamma, learning_rate, save_path, file_name, name)
+    def __init__(self, action_wrapper: ActionWrapper, state_builder: StateBuilder, save_path='urnai/models/saved/', file_name='dqltfflexible', learning_rate=0.0002, gamma=0.95, name='DQN', build_model = DqlTfFlexible.DEFAULT_BUILD_MODEL):
+        super(DqlTfFlexible, self).__init__(action_wrapper, state_builder, gamma, learning_rate, save_path, file_name, name)
 
         if save_path is None or file_name is None:
             raise TypeError
@@ -52,29 +53,31 @@ class DqlTfFlexible(LearningModel):
         self.decay_rate = 0.0001
         self.decay_step = 0
 
-        self.pickle_obj = [self.decay_step, self.nodes_layer1, self.nodes_layer2]
 
+        # ///// load_pickle should be called by the user! If it stays here, it may cause an accident if the user really wants a NEW OBJECT and at the same time he/she has a saved model inside the default folder, commenting this out!
         # Attempting to Load our serialized variables, as some of them will be used during the definition of our model
-        self.load_pickle()
-        
-        ops.reset_default_graph()
+        #self.load_pickle()
 
+        ops.reset_default_graph()
         tf.compat.v1.disable_eager_execution()
 
         # Initializing TensorFlow session
         self.sess = Session(config=ConfigProto(allow_soft_placement=True))
 
-        # Defining the model's layers
-        self.make_model(build_model)
+        # Defining the model's layers. Tensorflow's objects are stored into self.model_layers
+        self.build_model = build_model
+        self.make_model()
+
+        self.pickle_obj = [self.decay_step, self.build_model]
         
         #Not being used anywhere, commenting out:
         #self.actions_ = placeholder(dtype=tf.float32, shape=[None, self.action_size], name='actions_')
-
         
         self.sess.run(global_variables_initializer())
 
         self.saver = train.Saver()
-        self.load()
+        # The default behavior of the __init__() method should be to create a new object, if the user wants to load from the default folder, he/she may call the load method.
+        #self.load()
 
     def learn(self, s, a, r, s_, done, is_last_step: bool):
         qsa_values = self.sess.run(self.output_layer, feed_dict={self.inputs_: s})
@@ -132,45 +135,66 @@ class DqlTfFlexible(LearningModel):
         action = self.actions[int(action_idx)]
         return action
 
-    def save(self):
-        print()
-        print("> Saving the model!")
-        print()
-        self.saver.save(self.sess, self.save_path+self.file_name+"/"+self.file_name)
+    def get_full_persistance_pickle_path(self):
+        return self.save_path + self.file_name + os.path.sep + "model_" + self.file_name + ".pkl"
 
+
+    def get_full_persistance_tensorflow_path(self):
+        return self.save_path + os.path.sep + "model_tensorflow_" + self.file_name
+
+    def save(self):
+        print("Saving model...")
+
+        #Saving tensorflow stuff
+        self.saver.save(self.sess, self.get_full_persistance_tensorflow_path())
         # Dumping (serializing) decay_step into a pickle file
-        pickle_out = open(self.save_path+self.file_name+"/"+self.file_name+"_model.pickle", "wb")
-        self.pickle_obj = [self.decay_step, self.nodes_layer1, self.nodes_layer2]
-        pickle.dump(self.pickle_obj, pickle_out)
-        pickle_out.close()
+        with open(self.get_full_persistance_pickle_path(), "wb") as pickle_out: 
+            pickle.dump(self.pickle_obj, pickle_out)
 
     def load(self):
-        exists = os.path.isfile(self.save_path + self.file_name + "/" + self.file_name + '.meta')
+        #Load this model from persistant file
+        #Tensorflow variables need to be loaded separated,
+        #Because Session must be configured first. 
+        self.load_pickle()
+        self.make_model()
+        self.load_tf()
+
+    def load_tf(self)
+        #Check if tf file exists
+        exists = os.path.isfile(self.get_full_persistance_tensorflow_path + ".meta")
+        #If yes, load it
         if exists:
-            print()
-            print("> Loading saved model!")
-            print()
+            self.make_model()
             self.saver.restore(self.sess, self.save_path + self.file_name + "/" + self.file_name)
+        else:
+            #Else, raise exception
+            raise FileNotFoundError(self.get_full_persistance_tensorflow_path + " was not found.")
 
     def load_pickle(self):
-    # Loading (deserializing) a few parameters from a pickle file 
-        exists_pickle = os.path.isfile(self.save_path + self.file_name + "/" + self.file_name + '_model.pickle')
+        #Check if pickle file exists
+        exists_pickle = os.path.isfile(self.get_full_persistance_pickle_path)
+        #If yes, load it
         if exists_pickle:
-            pickle_in  = open(self.save_path + self.file_name + "/" + self.file_name + "_model.pickle", "rb")
-            self.pickle_obj = pickle.load(pickle_in)
-            self.decay_step = self.pickle_obj[0]
-            self.nodes_layer1 = self.pickle_obj[1]
-            self.nodes_layer2 = self.pickle_obj[2]
+            with open(self.get_full_persistance_pickle_path(), "wb") as pickle_in: 
+                self.pickle_obj = pickle.load(pickle_in)
+                self.decay_step = self.pickle_obj[0]
+                self.build_model = self.pickle_obj[1]
+        else:
+            #Else, raise exception
+            raise FileNotFoundError(self.get_full_persistance_tensorflow_path + " was not found.")
 
-    def make_model(self, build_model):
-        if build_model == DqlTfFlexible.DEFAULT_BUILD_MODEL:
-                    build_model[0]['shape'] = [None, self.state_size]
-                    build_model[3]['length'] = self.actions_size
+    def make_model(self):
+        #If the build model is the same as the default one, apply
+        #the default properties to input and output
+        if self.build_model == DqlTfFlexible.DEFAULT_BUILD_MODEL:
+                    self.build_model[0]['shape'] = [None, self.state_size]
+                    self.build_model[3]['length'] = self.actions_size
 
+        #Load each layer
         self.model_layers = []
-        for layer_model in build_model:
+        for layer_model in self.build_model:
             if layer_model['type'] == LAYER_INPUT: 
-                if build_model.index(layer_model) == 0:
+                if self.build_model.index(layer_model) == 0:
                     self.model_layers.append(placeholder(dtype=tf.float32, 
                         shape=layer_model['shape'], name='inputs_'))
                 else:
@@ -183,6 +207,7 @@ class DqlTfFlexible(LearningModel):
                 self.model_layers.append(layers.dense(inputs=self.model_layers[-1], 
                     units=self.action_size,activation=None))
 
+        #Setup output qsa layer and loss
         self.tf_qsa = placeholder(shape=[None, self.action_size], dtype=tf.float32)
         self.loss = tf.losses.mean_squared_error(self.tf_qsa, self.model_layers[-1])
         self.optimizer = train.AdamOptimizer(self.learning_rate).minimize(self.loss)
