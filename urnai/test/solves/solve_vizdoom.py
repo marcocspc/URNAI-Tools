c02d9d96dec4b69ecef6e8cfb566dc73054f63ce
marcocspc <marcocspc@hotmail.com>
2020-05-03 20:04:47 -0300

    allow more memory usage on tf_gpu

--- urnai/test/solves/solve_vizdoom.py
+++ urnai/test/solves/solve_vizdoom.py
@@ -22,8 +22,8 @@ from datetime import datetime
 #force tf_gpu to allow more memory usage
 #uncomment only if needed
 from keras.backend.tensorflow_backend import set_session
-import tensorflow as tf
-config = tf.compat.v1.ConfigProto()
+import tensorflow.compat.v1 as tf
+config = tf.ConfigProto()
 config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
 config.log_device_placement = True  # to log device placement (on which device the operation ran)sess = tf.Session(config=config)set_session(sess)  # set this TensorFlow session as the default session for Keras
 
