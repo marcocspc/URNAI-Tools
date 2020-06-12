9253fb103e1e4b7a5a25e9e66c6af6f965d90d86
marcocspc <marcocspc@hotmail.com>
2020-06-12 16:39:51 -0300

    putting a switch to install dependencies at their latest version if devs needs

--- setup.py
+++ setup.py
@@ -5,42 +5,43 @@ def is_optional_enabled(optional):
     return os.environ.get(optional, None) is not None
 
 # Dependencies versions
-# VERSION_TF = "==2.2.0"
-# VERSION_VIZDOOM = "==1.1.7"
-# VERSION_DRTS = "stable"
-# VERSION_PLE = "3dbe79d"
-# VERSION_2048 = "==0.2.6"
-# VERSION_ABSL = "==0.9.0"
-# VERSION_GYM = "==0.10.11"
-# VERSION_NUMPY = "==1.18.4"
-# VERSION_MATPLOTLIB = "==3.2.0"
-# VERSION_KERAS = "==2.3.1"
-# VERSION_PYSC2 = "==3.0.0"
-# VERSION_PANDAS = "==1.0.1"
-
-VERSION_TF = ""
-VERSION_VIZDOOM = ""
+VERSION_TF = "==2.2.0"
+VERSION_VIZDOOM = "==1.1.7"
 VERSION_DRTS = "stable"
-VERSION_PLE = ""
-VERSION_2048 = ""
-VERSION_ABSL = ""
-VERSION_GYM = ""
-VERSION_NUMPY = ""
-VERSION_MATPLOTLIB = ""
-VERSION_KERAS = ""
-VERSION_PYSC2 = ""
-VERSION_PANDAS = ""
-
+VERSION_PLE = "3dbe79d"
+VERSION_2048 = "==0.2.6"
+VERSION_ABSL = "==0.9.0"
+VERSION_GYM = "==0.10.11"
+VERSION_NUMPY = "==1.18.4"
+VERSION_MATPLOTLIB = "==3.2.0"
+VERSION_KERAS = "==2.3.1"
+VERSION_PYSC2 = "==3.0.0"
+VERSION_PANDAS = "==1.0.1"
 
 VIZDOOM = 'URNAI_VIZDOOM'
 TF_CPU = 'URNAI_TF_CPU'
 G2048 = 'URNAI_2048'
 DEEPRTS = 'URNAI_DEEPRTS'
+LATEST_DEPS = 'URNAI_LATEST_DEPS'
 
 git_url = '{package} @ git+https://github.com/{user}/{repo}@{branch}#egg={package}'
 dep_links = []
 dep_list = []
 tf = 'tensorflow-gpu' + VERSION_TF
+
+if is_optional_enabled(LATEST_DEPS):
+    print("Dependencies will be installed in theirs latest versions.")
+    VERSION_TF = ""
+    VERSION_VIZDOOM = ""
+    VERSION_2048 = ""
+    VERSION_ABSL = ""
+    VERSION_GYM = ""
+    VERSION_NUMPY = ""
+    VERSION_MATPLOTLIB = ""
+    VERSION_KERAS = ""
+    VERSION_PYSC2 = ""
+    VERSION_PANDAS = ""
+
 
 if is_optional_enabled(DEEPRTS):
     print("DeepRTS installation enabled.")
