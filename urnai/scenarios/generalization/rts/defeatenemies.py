fe2302716a30ca03a59526034ceaa762232c7a81
marcocspc <marcocspc@hotmail.com>
2020-07-16 03:24:15 -0300

    scenario 4 map ready

--- urnai/scenarios/generalization/rts/defeatenemies.py
+++ urnai/scenarios/generalization/rts/defeatenemies.py
@@ -72,8 +72,8 @@ class GeneralizedDefeatEnemiesScenario(GeneralizedFindaAndDefeatScenario):
                 ],
             }
 
-    def __init__(self, game = GAME_DEEP_RTS, render=False, drts_map="total-64x64-playable-21x13-defeatenemies.json", sc2_map="DefeatRoaches", drts_start_oil=999999, drts_start_gold=999999, drts_start_lumber=999999, drts_start_food=999999):
-        super().__init__(game=game, render=render, drts_map=drts_map, sc2_map=sc2_map, drts_number_of_players=2, drts_start_oil=drts_start_oil, drts_start_gold=drts_start_gold, drts_start_lumber=drts_start_lumber, drts_start_food=drts_start_food)
+    def __init__(self, game = GAME_DEEP_RTS, render=False, drts_map="total-64x64-playable-21x13-defeatenemies.json", sc2_map="DefeatRoaches", drts_number_of_players=2, drts_start_oil=999999, drts_start_gold=999999, drts_start_lumber=999999, drts_start_food=999999):
+        super().__init__(game=game, render=render, drts_map=drts_map, sc2_map=sc2_map, drts_number_of_players=drts_number_of_players, drts_start_oil=drts_start_oil, drts_start_gold=drts_start_gold, drts_start_lumber=drts_start_lumber, drts_start_food=drts_start_food)
 
     def step(self, action):
         if (self.game == GeneralizedDefeatEnemiesScenario.GAME_DEEP_RTS):
