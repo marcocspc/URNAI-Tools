2255a8d1f5983accf60d25901a5ba28469f1904f
marcocspc <marcocspc@hotmail.com>
2020-08-10 20:51:52 -0300

    starting scenario 4 actions

--- urnai/agents/actions/scenarios/rts/generalization/buildunits.py
+++ urnai/agents/actions/scenarios/rts/generalization/buildunits.py
@@ -61,38 +61,18 @@ class BuildUnitsStarcraftIIActionWrapper(DefeatEnemiesStarcraftIIActionWrapper):
     def __init__(self):
         super().__init__()
 
-        self.maximum_attack_range = 999999 
-        self.ver_threshold = 3
-        self.hor_threshold = 3
-
-        self.run = 5
-        self.actions = [self.attack, self.run]
+        self.collect_minerals = 6
+        self.build_supply_depot = 7
+        self.build_barrack = 8
+        self.build_marine = 9
+        self.actions = [self.collect_minerals, self.build_supply_depot, self.build_barrack, self.build_marine]
 
     def solve_action(self, action_idx, obs):
-        if action_idx == self.attack:
-            self.attack_(obs)
-        if action_idx == self.run:
-            self.run_(obs)
-        
-    def run_(self, obs):
-        #TODO
-        #get player x and y avg
-        avg_p_x, avg_p_y = self.get_race_unit_avg(obs, sc2_env.Race.terran)
-        #get enemy x and y avg
-        avg_e_x, avg_e_y = self.get_race_unit_avg(obs, sc2_env.Race.zerg)
-
-        final_x = 0
-        final_y = 0
-        if avg_p_x - avg_e_x < 0:
-            final_x = avg_p_x - self.hor_threshold
-        else:
-            final_x = avg_p_x + self.hor_threshold
-
-        if avg_p_y - avg_e_y < 0:
-            final_y = avg_p_y - self.ver_threshold
-        else:
-            final_y = avg_p_y + self.ver_threshold
-
-        army = scaux.select_army(obs, sc2_env.Race.terran)
-        for unit in army:
-            self.pending_actions.append(actions.RAW_FUNCTIONS.Move_pt("now", unit.tag, [final_x, final_y]))
+        if action_idx == self.collect_minerals:
+            self.collect(obs)
+        elif action_idx == self.build_supply_depot:
+            self.build_supply_depot_(obs)
+        elif action_idx == self.build_barrack:
+            self.build_barrack_(obs)
+        elif action_idx == self.build_marine:
+            self.build_marine_(obs)
