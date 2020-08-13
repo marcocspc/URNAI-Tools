f530b3a4dc7392bddd813f11e6bae621a831914c
marcocspc <marcocspc@hotmail.com>
2020-08-12 22:51:55 -0300

    all actions done for sc2 buildunits scenario, need some coordinates and test

--- urnai/agents/actions/scenarios/rts/generalization/buildunits.py
+++ urnai/agents/actions/scenarios/rts/generalization/buildunits.py
@@ -3,6 +3,7 @@ from .defeatenemies import DefeatEnemiesDeepRTSActionWrapper, DefeatEnemiesStarc
 from pysc2.lib import actions, features, units
 from statistics import mean
 from pysc2.env import sc2_env
+import random
 
 class BuildUnitsDeepRTSActionWrapper(DefeatEnemiesDeepRTSActionWrapper):
     def __init__(self):
@@ -58,6 +59,12 @@ class BuildUnitsDeepRTSActionWrapper(DefeatEnemiesDeepRTSActionWrapper):
 
 
 class BuildUnitsStarcraftIIActionWrapper(DefeatEnemiesStarcraftIIActionWrapper):
+
+    SUPPLY_DEPOT_X = -1
+    SUPPLY_DEPOT_Y = -1
+    BARRACK_X = -1
+    BARRACK_Y = -1
+
     def __init__(self):
         super().__init__()
 
@@ -76,3 +83,48 @@ class BuildUnitsStarcraftIIActionWrapper(DefeatEnemiesStarcraftIIActionWrapper):
             self.build_barrack_(obs)
         elif action_idx == self.build_marine:
             self.build_marine_(obs)
+        elif action_idx == self.stop:
+            self.pending_actions.clear()
+
+    def collect(self, obs):
+        #get SCV list
+        scvs = scaux.get_my_units_by_type(obs, units.Terran.SCV)
+        #get mineral list
+        mineral_fields = secaux.get_neutral_units_by_type(obs, units.Neutral.MineralField)
+        #split SCVs into sets of numberSCVs/numberOfMinerals
+        n = len(scvs)/len(mineral_fields)
+        scvs_sets = [scvs[i * n: (i + 1) * n] for i in range(len(scvs) + n - 1) // n)]
+        #make every set of SCVs collect one mineral 
+        for i in range(len(mineral_fields)):
+            mineral = mineral_fields[i]
+            scvset = scvs_sets[i]
+            for scv in scvset:
+    o            self.pending_actions.append(actions.RAW_FUNCTIONS.Harvest_Gather_unit("queued", scv.tag, mineral.tag))
+
+    def select_random_scv(self, obs):
+        #get SCV list
+        scvs = scaux.get_my_units_by_type(obs, units.Terran.SCV)
+        length = len(scvs)
+        scv = scvs[random.randint(0, length)] 
+        return scv
+
+    def build_supply_depot_(self, obs):
+        #randomly select scv
+        scv = self.select_random_scv(obs)
+        #get coordinates
+        x, y = SUPPLY_DEPOT_X, SUPPLY_DEPOT_X
+        #append action to build supply depot
+        self.pending_actions.append(actions.RAW_FUNCTIONS.Build_SupplyDepot_pt("now", scv.tag, [x, y]))
+
+    def build_barrack_(self, obs):
+        #randomly select scv
+        scv = self.select_random_scv(obs)
+        #get coordinates
+        x, y = BARRACK_X, BARRACK_Y 
+        #append action to build supply depot
+        self.pending_actions.append(actions.RAW_FUNCTIONS.Build_Barracks_pt("now", scv.tag, [x, y]))
+
+    def build_marine_(self, obs):
+        barracks = scaux.get_my_units_by_type(obs, units.Terran.Barracks)
+        for barrack in barracks:
+            self.pending_actions.append(actions.RAW_FUNCTIONS.Train_Marine_quick("now", barrack.tag))
