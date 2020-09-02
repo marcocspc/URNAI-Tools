bca4d04969e590321c76b54850985c6a72ffb926
marcocspc <marcocspc@hotmail.com>
2020-09-02 20:59:31 -0300

    trying to fix unicode issue on singularity

--- urnai/urnai_cmd.py
+++ urnai/urnai_cmd.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from urnai.runner.runnerbuilder import RunnerBuilder
 from urnai.runner.parserbuilder import ParserBuilder
-import os
+import sys, os
 
 def main():
     os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
     parser = ParserBuilder.DefaultParser()
     args = parser.parse_args()
