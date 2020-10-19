967e9ac6f43d81572658502a246b867ba8391dff
Marco Antonio <marcocspc@hotmail.com>
2020-10-18 21:06:44 -0300

    Update solve_simple64_veryeasy.py

--- urnai/solves/solve_simple64_veryeasy.py
+++ urnai/solves/solve_simple64_veryeasy.py
@@ -17,7 +17,9 @@ from models.model_builder import ModelBuilder
 
 def main(unused_argv):
     try:
-        env = SC2Env(map_name="Simple64", render=False, step_mul=16, player_race="terran", enemy_race="random", difficulty="very_easy")
+        env = SC2Env(map_name="Simple64", render=False, 
+                     step_mul=16, player_race="terran", 
+                     enemy_race="random", difficulty="very_easy")
         
         action_wrapper = SimpleTerranWrapper()
         state_builder = Simple64GridState(grid_size=4)
@@ -27,8 +29,13 @@ def main(unused_argv):
         helper.add_fullyconn_layer(50)
         helper.add_output_layer(action_wrapper.get_action_space_dim())
 
-        dq_network = DDQNKeras(action_wrapper=action_wrapper, state_builder=state_builder, build_model=helper.get_model_layout(), per_episode_epsilon_decay=False,
-                                gamma=0.99, learning_rate=0.001, epsilon_decay=0.99999, epsilon_min=0.005, memory_maxlen=100000, min_memory_size=2000)
+        dq_network = DDQNKeras(action_wrapper=action_wrapper, 
+                               state_builder=state_builder, 
+                               build_model=helper.get_model_layout(), 
+                               per_episode_epsilon_decay=False,
+                                gamma=0.99, learning_rate=0.001, 
+                               epsilon_decay=0.99999, epsilon_min=0.005, 
+                               memory_maxlen=100000, min_memory_size=2000)
         
         agent = SC2Agent(dq_network, KilledUnitsReward())
         
